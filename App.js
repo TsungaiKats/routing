405da4207673469ef1a9c3b007c1718cf284bd8e
Tsungai Katsuro OW <101188314+TsungaiKats@users.noreply.github.com>
2025-03-06 12:35:09 +0200

    Update App.js

--- App.js
+++ App.js
@@ -4,13 +4,13 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import BarChart from "./components/BarChart";
+
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <BarChart />
+     
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
